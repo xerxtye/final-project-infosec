18024d67b604338eb1891fd6223791b5d16e38c7
kylzhirov <kr12488@auca.kg>
2026-03-23 09:52:48 +0600

    Add time range select options

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import news_scraper
+from datetime import datetime, time, timedelta
 
 if __name__ == '__main__':
-    print('List of available sources: ')
-    print('Enter the news source: ')
-    news_source = input()
-    print('Enter the date in yyyy-mm-dd format')
-    date = input()
+    news_source = input('Enter the news source: ')
+
+    last_three_days = (datetime.now() - timedelta(days=3)).strftime('%Y-%m-%d') 
+    week = (datetime.now() - timedelta(days=7)).strftime('%Y-%m-%d')
+    month = (datetime.now() - timedelta(days=30)).strftime('%Y-%m-%d') 
+
+    time_periods = {} 
+    time_periods[1] = last_three_days
+    time_periods[2] = week
+    time_periods[3] = month
+
+    print("Select the time period: ")
+    print("1. Last Three Days")
+    print("2. Week")
+    print("3. Month")
+    date = time_periods[int(input())]
 
     news_scraper.get_news(news_source, date)
--- news_scraper.py
+++ news_scraper.py
@@ -6,9 +6,6 @@ config = configparser.ConfigParser()
 config.read('config.ini')
 api_key = config['newsapi']['api_key']
 
-news_source = ' '
-date = '2026-03-15'
-
 def get_news(news_source, date):
     url = ('https://newsapi.org/v2/everything?'
         f'q={news_source}&'
